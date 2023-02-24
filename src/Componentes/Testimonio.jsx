1ffd39da0a7bc5dbaa128409804bd974b324d41a
fortis-a <andrea.fortisb@gmail.com>
2023-02-24 00:52:07 -0600

    changes

---
+++ src/Componentes/Testimonio.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import "../styleSheets/Testimonio.css"
+
+export function Testimonio (props) {
+
+
+  return (
+    <div className='contenedor-testimonio'>
+      <img className='imagen-testimonio' src={require(`../imagenes/testimonio-${props.imagen}.png`)} alt='Foto de emma' />
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> in {props.pais}</p>
+        <p className='cargo-testimonio'>
+          {props.cargo} at <strong>{props.empresa}</strong></p>
+        <p className='texto-testimonio'>"{props.testimonio}"</p>
+      </div>
+    </div>
+  
+  )
+}
+
+export default Testimonio
+
